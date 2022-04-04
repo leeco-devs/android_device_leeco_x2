178be636f44386aa90c991d0a9dca0c73972ade0
Lucchetto <lucchetto.tie@live.com>
2022-04-04 19:36:29 +0200

    x2: build PixelExperience
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- aosp_x2.mk
+++ aosp_x2.mk
@@ -21,14 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PE stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := LeEco
 PRODUCT_DEVICE := x2
 PRODUCT_MANUFACTURER := LeEco
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := aosp_x2
 
 PRODUCT_GMS_CLIENTID_BASE := android-leeco
 
